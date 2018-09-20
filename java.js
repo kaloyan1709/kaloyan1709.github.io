9dcec28de2056572fcb79f7e3f63dca980279493
Kaloyan Pashov <kaloyan1709@gmail.com>
2018-09-20 19:55:35 +0300

    New
    Adding a new feature that let's you show the hidden qr code

--- java.js
+++ java.js
@@ -1,3 +1,8 @@
-function myFunction() {
-    document.getElementById("demo").innerHTML = "Paragraph changed.";
+function myFunction()  {
+	var x = document.getElementById('btc');
+	 if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
 }
